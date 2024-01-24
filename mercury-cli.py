a7aeb2c8555e9e7c6e44ed41e2f3a0379110a7c0
Maksim Balukh <maxbsoft@gmail.com>
2024-01-24 23:30:04 +0200

    fix

--- mercury-cli.py
+++ mercury-cli.py
@@ -6,87 +6,110 @@ import time
 import requests
 import csv
 import io
-from cassandra.cluster import Cluster, NoHostAvailable, Session
+from cassandra.cluster import Cluster, NoHostAvailable, Session, ResultSet
 from cassandra.auth import PlainTextAuthProvider
-from cassandra import ReadTimeout, WriteTimeout, InvalidRequest, Unauthorized, AuthenticationFailed
-from cassandra.query import BatchStatement, BatchType
+from cassandra import ReadTimeout, WriteTimeout, InvalidRequest, Unauthorized, AuthenticationFailed, ConsistencyLevel
+from cassandra.query import BatchStatement, BatchType, SimpleStatement
 from cassandra.concurrent import execute_concurrent_with_args
 
 def main():
-    parser = argparse.ArgumentParser(description="ScyllaDB performance test")
+    parser = argparse.ArgumentParser(description="Mercury CLI")
 
-    # Mandatory argument for selecting the test type
-    parser.add_argument('test_type', choices=['random', 'fromfile'], help='Test type: random or fromfile')
+    # Mandatory argument for selecting the command
+    parser.add_argument(
+        'command',
+        choices=['createdb', 'fillbaselist', 'removedata', 'checkbaselist'],
+        help='Command type: createdb, removedata or fillbaselist'
+    )
 
     # Optional arguments with default values
-    parser.add_argument('--row-count', type=int, default=1000000, help='Number of data rows (default: 1000000)')
+    parser.add_argument('--keyspace', default='mercure', help='Keyspace for the scylladb database  (default: mercure)')
     parser.add_argument('--db', default='127.0.0.1', help='IP address of ScyllaDB database (default: 127.0.0.1)')
     parser.add_argument('--db-port', type=int, default=9042, help='ScyllaDB database port (default: 9042)')
-    parser.add_argument('--threads', type=int, default=1, help='Number of threads for parallel recording (default: 1)')
 
-    # Argument for the test fromfile
-    parser.add_argument('--data-list-file', help='Path to the test data file fromfile')
+
+    parser.add_argument('--base-list-file', help='Path to the list data file')
+    parser.add_argument('--base-list-group', type=int, help='Indicates which data group or source RY is 1, RP is 2, etc...')
 
     args = parser.parse_args()
 
-    # Logic for various tests
-    if args.test_type == 'random':
-        run_random_test(args)
-    elif args.test_type == 'fromfile':
-        run_fromfile_test(args)
+    # Logic for various commands
+    if args.command == 'createdb':
+        run_createdb(args)
+    elif args.command == 'fillbaselist':
+        run_fillbaselist(args)
+    elif args.command == 'removedata':
+        run_removedata(args)
+    elif args.command == 'checkbaselist':
+        run_checkbaselist(args)
     else:
-        print("Unknown test type")
+        print("Unknown command type")
         sys.exit(1)
 def connect_to_scylladb(ip, port):
     # auth_provider = PlainTextAuthProvider(username='your_username', password='your_password')
     # cluster = Cluster([ip], port=port, auth_provider=auth_provider)
     cluster = Cluster([ip], port=port)
     session = cluster.connect()
-    print(f"Имя кластера: {cluster.metadata.cluster_name}")
+    print(f"Cluster name: {cluster.metadata.cluster_name}")
+    # print(f"Cluster name: {cluster.metadata}")
     return session, cluster
 
 def disconnect_from_scylladb(session, cluster):
     session.shutdown()
     cluster.shutdown()
 
-def create_test_table(session):
-    session.execute("""
-        CREATE KEYSPACE IF NOT EXISTS testkeyspace
-        WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};
+def create_db_structure(session, keyspace):
+    print(f"Try to create keyspace: {keyspace} if it does not exist...")
+    result = session.execute(f"""
+        CREATE KEYSPACE IF NOT EXISTS {keyspace}
+        WITH replication = {{'class': 'SimpleStrategy', 'replication_factor' : 1}};
     """)
-    session.set_keyspace("testkeyspace")
-    session.execute("""
-        CREATE TABLE IF NOT EXISTS testtable (
-            id blob PRIMARY KEY,
-            data text
-        ) WITH compression = {};
+    print(f"result:{result}")
+    print(f"Set keyspace: {keyspace}")
+    result = session.set_keyspace(keyspace)
+    print(f"result:{result}")
+
+    print("Trying to create a baselist table...")
+    result = session.execute("""
+        CREATE TABLE IF NOT EXISTS baselist (
+            data text PRIMARY KEY,
+            group smallint
+        )
     """)
-    
-    
-def cleanup_test_data(session):
+    # WITH compression = {};
+    print(f"result:{result}")
+
     # try:
-    #     # Пытаемся выполнить запрос SELECT к таблице
-    #     session.execute("SELECT * FROM testkeyspace.testtable LIMIT 1;")
-    #     # Если запрос выполнен успешно, таблица существует, и мы можем ее очистить
-    #     session.execute("TRUNCATE testkeyspace.testtable;", timeout=60)
-    # except InvalidRequest:
-    #     # Исключение InvalidRequest будет вызвано, если таблица не существует
-    #     print("Таблица не существует, пропускаем TRUNCATE.")
-    session.execute("DROP KEYSPACE IF EXISTS testkeyspace;", timeout=360)
+    #     print("Trying to create an index for baselist (group)...")
+    #     result = session.execute("CREATE INDEX ON baselist (group);")
+    #     print(f"result:{result}")
+    # except InvalidRequest:  # Замените на соответствующее исключение
+    #     print("Index already exists.")
+    
+# def cleanup_test_data(session):
+#     # try:
+#     #     # Пытаемся выполнить запрос SELECT к таблице
+#     #     session.execute("SELECT * FROM testkeyspace.testtable LIMIT 1;")
+#     #     # Если запрос выполнен успешно, таблица существует, и мы можем ее очистить
+#     #     session.execute("TRUNCATE testkeyspace.testtable;", timeout=60)
+#     # except InvalidRequest:
+#     #     # Исключение InvalidRequest будет вызвано, если таблица не существует
+#     #     print("Таблица не существует, пропускаем TRUNCATE.")
+#     session.execute("DROP KEYSPACE IF EXISTS testkeyspace;", timeout=360)
 
-def generate_test_data(row_count):
-    test_data = []
-    for i in range(row_count):
-        # Генерация 32-байтового уникального идентификатора
-        id_bytes = os.urandom(32)
+# def generate_test_data(row_count):
+#     test_data = []
+#     for i in range(row_count):
+#         # Генерация 32-байтового уникального идентификатора
+#         id_bytes = os.urandom(32)
 
-        # Создание строки данных
-        data_str = f"Test performance, тестdata {i}"
+#         # Создание строки данных
+#         data_str = f"Test performance, тестdata {i}"
 
-        # Добавление кортежа (id, data) в список
-        test_data.append((id_bytes, data_str))
+#         # Добавление кортежа (id, data) в список
+#         test_data.append((id_bytes, data_str))
 
-    return test_data
+#     return test_data
 
 # def insert_data(session, data_chunk):
 #     batch = BatchStatement()
@@ -105,53 +128,27 @@ def generate_test_data(row_count):
 #        batch.add("INSERT INTO testtable (id, data) VALUES (%s, %s)", (id_bytes, data_str))
 #     session.execute(batch, timeout = 120)
     
-def insert_data(session: Session, data_chunk):
-    # batch = BatchStatement(BatchType.UNLOGGED)
-    for id_bytes, data_str in data_chunk:
-        session.execute("INSERT INTO testtable (id, data) VALUES (%s, %s)", (id_bytes, data_str))
+# def insert_data(session: Session, data_chunk):
+#     # batch = BatchStatement(BatchType.UNLOGGED)
+#     for id_bytes, data_str in data_chunk:
+#         session.execute("INSERT INTO testtable (id, data) VALUES (%s, %s)", (id_bytes, data_str))
 
-def worker(session, data_chunk, counter):
-    start_time = time.time()
-    insert_data(session, data_chunk)
-    elapsed_time = time.time() - start_time
-    rows_inserted = len(data_chunk)
-    counter.append(rows_inserted)
-    print(f"Поток завершил вставку {rows_inserted} строк за {elapsed_time:.2f} секунд. Скорость: {rows_inserted/elapsed_time:.2f} строк/сек")
+# def worker(session, data_chunk, counter):
+#     start_time = time.time()
+#     insert_data(session, data_chunk)
+#     elapsed_time = time.time() - start_time
+#     rows_inserted = len(data_chunk)
+#     counter.append(rows_inserted)
+#     print(f"Поток завершил вставку {rows_inserted} строк за {elapsed_time:.2f} секунд. Скорость: {rows_inserted/elapsed_time:.2f} строк/сек")
 
-def run_random_test(args):
-    # Here will be the logic for the random test
-    print(f"Running a 'random' test with {args.row_count} rows")
+def run_createdb(args):
+    print(f"Running a 'createdb' structure with keyspace:{args.keyspace}")
     try:
         print(f"Connecting to ScyllaDB {args.db}:{args.db_port}...")
         session, cluster = connect_to_scylladb(args.db, args.db_port)
-        cleanup_test_data(session)
-
-        print(f"Creating test table...")
-        create_test_table(session)
-
-        # Here will be the logic for inserting the data
-        # Генерация тестовых данных
-        test_data = generate_test_data(args.row_count)
-
-        start_time = time.time()
-         # Разбиваем данные на части для потоков
-        chunks = [test_data[i::args.threads] for i in range(args.threads)]
-        
-        threads = []
-        counter = []
-        for chunk in chunks:
-            t = threading.Thread(target=worker, args=(session, chunk, counter))
-            threads.append(t)
-            t.start()
-
-        for t in threads:
-            t.join()
-        elapsed_time = time.time() - start_time
-        total_rows = sum(counter)
-        print(f"Всего вставлено {total_rows} строк за {elapsed_time} сек. Скорость: {total_rows/elapsed_time:.2f} строк/сек")
-
-        rows = session.execute("SELECT COUNT(1) FROM testtable", timeout=500)
-        print("rows:", rows[0])
+       
+        print(f"Creating db structure...")
+        create_db_structure(session, args.keyspace)
 
     except NoHostAvailable:
         print(f"Connection error: Failed to connect to ScyllaDB at {args.db}:{args.db_port}. Check the IP address and port.")
@@ -166,60 +163,143 @@ def run_random_test(args):
     except Exception as e:
         print(f"Unknown error: {e}")
     finally:
-        # Clear test data and disconnect even if errors occurred
+        # Disconnect even if errors occurred
         if 'session' in locals() and 'cluster' in locals():
-            print(f"Cleanup test data...")
-            cleanup_test_data(session)
             print(f"Disconnecting from scylladb...")
             disconnect_from_scylladb(session, cluster)
 
-def run_fromfile_test(args):
+def run_removedata(args):
+    print(f"Running a 'run_removedata' with keyspace:{args.keyspace}")
+    try:
+        print(f"Connecting to ScyllaDB {args.db}:{args.db_port}...")
+        session, cluster = connect_to_scylladb(args.db, args.db_port)
+       
+        session.execute(f"DROP KEYSPACE IF EXISTS {args.keyspace};", timeout=360)
+        # create_db_structure(session, args.keyspace)
+
+    except NoHostAvailable:
+        print(f"Connection error: Failed to connect to ScyllaDB at {args.db}:{args.db_port}. Check the IP address and port.")
+    except AuthenticationFailed:
+        print("Authentication error: Incorrect credentials.")
+    except (ReadTimeout, WriteTimeout):
+        print("Timeout error: The read/write operation took too long.")
+    except InvalidRequest as e:
+        print(f"Query execution error: {e}")
+    except Unauthorized:
+        print("Access error: insufficient permissions to perform the operation.")
+    except Exception as e:
+        print(f"Unknown error: {e}")
+    finally:
+        # Disconnect even if errors occurred
+        if 'session' in locals() and 'cluster' in locals():
+            print(f"Disconnecting from scylladb...")
+            disconnect_from_scylladb(session, cluster)
+
+def run_fillbaselist(args):
     # Here is the logic for the fromfile test
-    print(f"Running the 'fromfile' test with a file {args.data_list_file}")
+    print(f"Running the 'fillbaselist' command fills the data into the baselist table from the {args.base_list_file} file. Keyspace:{args.keyspace}")
+    try:
+        total_lines = 2000000000 # sum(1 for line in open(args.base_list_file, 'r', encoding='ascii', errors='replace'))
+        # for line in open(args.base_list_file, 'r', errors='ignore'):
+        #     total_lines = total_lines + 1
+        #     if total_lines % 1000000 == 0:
+        #         print("Lines count:", total_lines)
+        # print(f"Total lines in file: {total_lines}")
+
+        processed_lines = 0
+        additional_data={
+            'keyspace': args.keyspace,
+            'table': 'baselist'
+        }
+        with open(args.base_list_file, 'r', encoding='utf-8', errors='replace') as file:
+            csv_output = io.StringIO()
+            writer = csv.writer(csv_output, quotechar='"', quoting=csv.QUOTE_ALL)
+
+            count = 0
+            skip = 0
+
+            for line in file:
+                
+                stripped_line = line.strip()
+                escaped_line = stripped_line.replace("\\", "\\\\")
+                escaped_line = escaped_line.replace(",", "\,")
+                # Преобразование строки файла в формат CSV (подставьте нужные данные)
+                if escaped_line:
+                    skip = skip + 1
+                    if skip < 237500000:
+                        continue
+                    writer.writerow([escaped_line, args.base_list_group])
+                else:
+                    continue
+
+                count += 1
+                processed_lines += 1
+                if count >= 500000:  # Проверка на достижение порога в 1 миллион строк
+                    # Отправка данных
+                    csv_output.seek(0)
+                    isComplete = False
+                    while isComplete == False:
+                        print("Try upload csv...")
+                        response = requests.post(
+                            f"http://{args.db}:5001/upload_csv",
+                            files={"file": csv_output},
+                            data=additional_data
+                        )
+                        if response.status_code == 200:
+                            isComplete = True
+                        else:
+                            print(f"Error: {response.text}")
+                            print(f"Neet repeat. Wait 10 seconds...")
+                            time.sleep(10)
+                     
+
+
+
+                    # Отображение ответа сервера
+                    print(f"Processed {processed_lines} of {total_lines} lines. Response: {response.text}")
+                    
+                    # Сброс счётчика и очистка буфера
+                    count = 0
+                    csv_output.seek(0)
+                    csv_output.truncate(0)
+
+            # Отправка оставшихся данных (если они есть)
+            if count > 0:
+                csv_output.seek(0)
+                response = requests.post(
+                    f"http://{args.db}:5001/upload_csv",
+                    files={"file": csv_output},
+                    data=additional_data
+                )
+                print(f"Processed {processed_lines} of {total_lines} lines. Response: {response.text}")
+
+    except FileNotFoundError:
+        print(f"File not found: {args.base_list_file}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print(e)
+
+def run_checkbaselist(args):
+    print(f"Running the 'checkbaselist' command. Keyspace:{args.keyspace}")
     try:
         print(f"Connecting to ScyllaDB {args.db}:{args.db_port}...")
         session, cluster = connect_to_scylladb(args.db, args.db_port)
-        cleanup_test_data(session)
+        session.set_keyspace(args.keyspace)
+        start = time.time()
+        query = SimpleStatement("SELECT * FROM baselist WHERE data='!root'", consistency_level=ConsistencyLevel.LOCAL_ONE)
+        rows: ResultSet = session.execute(query, timeout=200)
+        count = 0
+        print('len(rows):', rows.one())
+        print('time:', time.time() - start)
+        # rows.fetch_next_page()
 
-        print(f"Creating test table...")
-        create_test_table(session)
-
-        # Here will be the logic for inserting the data
-        # Генерация тестовых данных
-        test_data = generate_test_data(args.row_count)
-
-        output = io.StringIO()
-        writer = csv.writer(output)
-
-        for row in test_data:
-            writer.writerow([row[0].hex(), row[1]])
-
-        # Перемещение указателя в начало файла
-        output.seek(0)
-
-        # Словарь для передачи дополнительных данных
-        additional_data = {
-            'keyspace': 'testkeyspace',
-            'table': 'testtable'
-        }
-
-        start_time = time.time()
-
-        response = requests.post(
-            f"http://{args.db}:5001/upload_csv",
-            files={"file": output},
-            data=additional_data
-        )
-
-        # Вывод ответа сервера
-        print(response.text)
-
-        elapsed_time = time.time() - start_time
-        total_rows = args.row_count
-        print(f"Всего вставлено {total_rows} строк за {elapsed_time} сек. Скорость: {total_rows/elapsed_time:.2f} строк/сек")
-
-        rows = session.execute("SELECT COUNT(1) FROM testtable", timeout=500)
-        print("rows:", rows[0])
+        # for row in rows:
+        #     count = count + 1
+        #     if count % 1000 == 0:
+        #         print('count:', count)
+        # print('count:', count)
+        # print(f"Creating db structure...")
+        # create_db_structure(session, args.keyspace)
 
     except NoHostAvailable:
         print(f"Connection error: Failed to connect to ScyllaDB at {args.db}:{args.db_port}. Check the IP address and port.")
@@ -234,12 +314,11 @@ def run_fromfile_test(args):
     except Exception as e:
         print(f"Unknown error: {e}")
     finally:
-        # Clear test data and disconnect even if errors occurred
+        # Disconnect even if errors occurred
         if 'session' in locals() and 'cluster' in locals():
-            print(f"Cleanup test data...")
-            cleanup_test_data(session)
             print(f"Disconnecting from scylladb...")
             disconnect_from_scylladb(session, cluster)
+    
 
 if __name__ == "__main__":
     main()
